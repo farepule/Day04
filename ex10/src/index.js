0b79f6a9ccc6900d40a463df28142711dd13696c
farepule <faruk.puska@gmail.com>
2021-03-11 22:08:01 +0100

    Me

---
+++ ex10/src/index.js
@@ -0,0 +1,20 @@
+function main(){
+  
+  //Only change code below this line
+  var myFirstName = "Faruk";
+  var myLastName = "Puska"
+  var myHomeTown = "Sarajevo"
+  //Only change code above this line  
+  return {
+
+    myFirstName,
+    myLastName,
+    myHomeTown
+    
+  };
+
+}
+
+console.log(main())
+
+module.exports = main;
